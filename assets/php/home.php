3967a56b7dc10dd87aade0f1a0727fe230b971d0
RobLucci12 <cesartn12@gmail.com>
2021-04-29 15:50:55 -0500

    Cambio Sesiones
    Se añadieron los cambios

--- assets/php/home.php
+++ assets/php/home.php
@@ -5,7 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-        <h1>Bienvenido al sistema de encuestas</h1>
+        <label>asdlkfjasdñlfkjñlk</label>
+
+
 
 </head>
 <body>
