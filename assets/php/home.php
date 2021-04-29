e539350a050abe059568688a6ce6f859cb167f60
Jcnt98 <a13001697@alumnos.uady.mx>
2021-04-29 15:57:23 -0500

    Correccion Cesar
    Cambio del texto del label.

--- assets/php/home.php
+++ assets/php/home.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inicio</title>
-        <label>asdlkfjasdñlfkjñlk</label>
+        <label>Esta es la correccion de la mamada que hiz cesar</label>
 
 
 
