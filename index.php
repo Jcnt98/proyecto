e0650bc922f4bcc6eac7e27df30a2b5446465067
RobLucci12 <cesartn12@gmail.com>
2021-05-19 17:58:25 -0500

    Cambios Cesar
    Se cambio la estructura del index y de la pagina principal

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
 
 		$pagina=$_GET["page"];
 
-		if($pagina=="login") include "pages/login.html";
+		if($pagina=="login") include "assets/pages/login.html";
 
-		if($pagina=="agenda") include "pages/evaluacion.html";
+		if($pagina=="agenda") include "assets/pages/evaluacion.html";
 
-		if($pagina=="cuestionario") include "pages/cuestionario.html";
+		if($pagina=="cuestionario") include "assets/pages/cuestionario.html";
 
 	}else{
 
 		$pagina="login";
 
-		include "php/login.html";
+		include "assets/pages/login.html";
 
 	}//Fin del else...
 
